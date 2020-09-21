f85c13be0c238d558e43107ca8fc188c3ffabc1c
Gurina Elena <gurina_elena@inbox.ru>
2020-09-21 12:15:22 +0300

    Изменения в тесте

--- src/test/java/FirstTestWithSQL.java
+++ src/test/java/FirstTestWithSQL.java
@@ -1,4 +1,3 @@
-
 import lombok.val;
 import org.apache.commons.dbutils.QueryRunner;
 import org.apache.commons.dbutils.handlers.ScalarHandler;
@@ -9,8 +8,6 @@ import org.junit.jupiter.api.Test;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -33,30 +30,30 @@ public class FirstTestWithSQL {
                 val conn = DriverManager.getConnection(
                         "jdbc:mysql://localhost:3306/app", "app", "pass"
                 );
-        ) {
+       ) {
             val finishRunner = new QueryRunner();
 
-            val delCode = "delete from auth_code";
+            val delCode = "delete from auth_codes";
             val delCards = "delete from cards";
             val delUser = "delete from users";
 
-            val deleteCode = finishRunner.query(conn, delCode, new ScalarHandler<>());
-            val deleteCards = finishRunner.query(conn, delCards, new ScalarHandler<>());
-            val deleteUser = finishRunner.query(conn, delUser, new ScalarHandler<>());
+            val deleteCode = finishRunner.update(conn, delCode);
+            val deleteCards = finishRunner.update(conn, delCards);
+            val deleteUser = finishRunner.update(conn, delUser);
             System.out.println(deleteCode);
             System.out.println(deleteCards);
             System.out.println(deleteUser);
 
-        }
-    }
+       }
+   }
 
     @Test
     void stubTest() throws SQLException {
 
         val runner = new QueryRunner();
 
-        val loginSQL = "SELECT login FROM users where id ='3f435e31-9d10-494c-b129-12880cf62113'";
-        val codeSQL ="SELECT code FROM auth_codes ORDER BY created DESC  limit 1";
+        val loginSQL = "SELECT login FROM users where id ='65653d07-5845-40f6-9eaf-6ed754a68455'";
+        val codeSQL = "SELECT code FROM auth_codes ORDER BY created DESC  limit 1";
 
 
         try (
@@ -68,17 +65,15 @@ public class FirstTestWithSQL {
 
             String loginUser = runner.query(conn, loginSQL, new ScalarHandler<>());
             System.out.println(loginUser);
-            String codeUser = runner.query(conn, codeSQL, new ScalarHandler<>() );
+            String codeUser = runner.query(conn, codeSQL, new ScalarHandler<>());
 
             open("http://localhost:9999");
             $("[data-test-id=login] input").setValue(loginUser);
             $("[data-test-id=password] input").setValue("qwerty123");
             $(".button").click();
-            $(withText("Сохранить")).waitUntil (visible, 20000);
-            $(withText("Нет")).click();
-            $(withText("Необходимо подтверждение")).waitUntil (visible, 50000);
             $("[data-test-id=code] input").setValue(codeUser);
 
         }
+
     }
-}
+    }
